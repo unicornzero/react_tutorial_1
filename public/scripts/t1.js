3a8aa84c31b5ec6f91530f4f17ab46be3e93320a
Rylee <rylee@rallydev.com>
2015-06-10 10:45:26 -0600

    dynamic comment nodes

--- public/scripts/t1.js
+++ public/scripts/t1.js
@@ -4,7 +4,7 @@ var CommentBox = React.createClass({
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
@@ -13,9 +13,16 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+          </Comment>
+        );
+    });
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        {commentNodes}
       </div>
     );
   }
@@ -31,8 +38,27 @@ var CommentForm = React.createClass({
   }
 });
 
+var Comment = React.createClass({
+  render: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+      </div>
+      );
+  }
+});
+
+var data = [
+  {author: "Pete Hunter", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 React.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('content')
 );
 
